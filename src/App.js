29f8484a8cc1b8d3e93e7342ce8562c7fb05fc7e
synerzip <sanketjoshi96@gmail.com>
2019-07-09 14:24:44 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
 import Palette from './palette';
+import PaletteList from './paletteList'
 import palettes from './seedColor';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { generatePalette } from './colorHelper';
+
+
+function getPaletteById(id) {
+  const rawPalette = palettes.find(palette => {
+    return palette.id === id;
+  });
+  return generatePalette(rawPalette);
+}
+
 
 function App() {
   return (
     <div className="App">
-      <Palette data={generatePalette(palettes[4])}></Palette>
+      <Router>
+        <Switch>
+          <Route path="/" exact render={() => <PaletteList data={palettes}></PaletteList>}></Route>
+          <Route path="/palette/:id" exact render={(args) => {
+            return <Palette data={getPaletteById(args.match.params.id)}></Palette>
+          }}>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/paletteList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class PaletteList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() {
+
+        return (  
+            <div>List</div>
+        );
+    }
+}
+ 
+export default PaletteList;
